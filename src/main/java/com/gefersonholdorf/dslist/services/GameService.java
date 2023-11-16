a663c13251e43b6ffd96935fa0957770b23f4351
Geferson Holdorf <gefersonholdorf88@gmail.com>
2023-11-16 17:19:25 -0300

    Get game by id

--- src/main/java/com/gefersonholdorf/dslist/services/GameService.java
+++ src/main/java/com/gefersonholdorf/dslist/services/GameService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gefersonholdorf.dslist.dto.GameDTO;
 import com.gefersonholdorf.dslist.dto.GameMinDTO;
 import com.gefersonholdorf.dslist.entities.Game;
 import com.gefersonholdorf.dslist.repositories.GameRepository;
@@ -20,6 +21,12 @@ public class GameService {
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
     
 }
